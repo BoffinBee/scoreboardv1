fb480a1f37f790284d00406416302f01712e4689
BoffinBee <boffinbee@gmail.com>
2022-07-29 10:17:41 +0100

    Added some comments

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 A built from scratch copy of the design at https://www.figma.com/file/YC48MCx4frBFtYoz6rNJE6/Basketball-Scoreboard?node-id=0%3A1 but added a reset button to clear current scores and get it back to zero.
 
+Everything done now with pure HTML, CSS, and JavaScript.
+Probably will be improved in the future!
 
-©️BoffinBee, July 2022
+©️BoffinBee, July 2022.
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-let homeScore = 0;
-let awayScore = 0;
-
+// GRAB THE SCORE ELEMENTS
 const homeScoreEl = document.getElementById("home-score");
 const awayScoreEl = document.getElementById("away-score");
 
+// SET THE SCORE COUNTER FOR BOTH TEAMS
+let homeScore = 0;
+let awayScore = 0;
+
+// RESET THE SCORE TO ZERO
 function resetScore() {
   homeScore = 0;
   awayScore = 0;
@@ -11,31 +14,37 @@ function resetScore() {
   awayScoreEl.innerHTML = awayScore;
 }
 
+// ADD ONE TO THE HOME SCORE
 function homeAddOne() {
   homeScore += 1;
   homeScoreEl.innerHTML = homeScore;
 }
 
+// ADD TWO TO THE HOME SCORE
 function homeAddTwo() {
   homeScore += 2;
   homeScoreEl.innerHTML = homeScore;
 }
 
+// ADD THREE TO HOME SCORE
 function homeAddThree() {
   homeScore += 3;
   homeScoreEl.innerHTML = homeScore;
 }
 
+// ADD ONE TO AWAY SCORE
 function awayAddOne() {
   awayScore += 1;
   awayScoreEl.innerHTML = awayScore;
 }
 
+// ADD TWO TO AWAY SCORE
 function awayAddTwo() {
   awayScore += 2;
   awayScoreEl.innerHTML = awayScore;
 }
 
+// ADD THREE TO AWAY SCORE
 function awayAddThree() {
   awayScore += 3;
   awayScoreEl.innerHTML = awayScore;
